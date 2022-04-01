b1e7972e9ba9ad0260549790377439f8ebd3ffa0
jagveer <jagveersingh.55555@gmail.com>
2022-04-01 21:56:42 +0530

    UC2

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -6,17 +6,27 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Pattern pattern;
-		String firstName;
+		String firstName, lastName;
 		Matcher matcher;
 
 		pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		System.out.println("Enter the FirstName");
 		while (true) {
-			System.out.println("Enter the FirstName");
 			firstName = scanner.nextLine();
 			if (pattern.matcher(firstName).matches()) {
 				break;
 			}
 			System.out.println("Enter valid first name");
+		}
+
+		pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		System.out.println("Enter the Last Name");
+		while (true) {
+			lastName = scanner.nextLine();
+			if (pattern.matcher(lastName).matches()) {
+				break;
+			}
+			System.out.println("Enter valid last name");
 		}
 	}
 }
