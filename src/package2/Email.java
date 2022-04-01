13a04817c7ef0247753c3ea77ab207434d22f972
jagveer <jagveersingh.55555@gmail.com>
2022-04-01 22:45:23 +0530

    email uc1

---
+++ src/package2/Email.java
@@ -0,0 +1,19 @@
+package package2;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Email {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter email");
+		Pattern pattern = Pattern.compile("^abc");
+		Matcher matcher = pattern.matcher(scanner.nextLine());
+		if (matcher.matches()) {
+			System.out.println("Input matches format");
+		} else {
+			System.out.println("Input doesn't match format");
+		}
+	}
+}
