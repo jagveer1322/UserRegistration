6726283349ba9d9b7873570d0e70109a8f6a499a
jagveer <jagveersingh.55555@gmail.com>
2022-04-02 07:31:53 +0530

    pincode uc1

---
+++ src/package2/PinCode.java
@@ -0,0 +1,19 @@
+package package2;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinCode {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Pin Code");
+		Pattern pattern = Pattern.compile("\\d{6}");
+		Matcher matcher = pattern.matcher(scanner.nextLine());
+		if (matcher.matches()) {
+			System.out.println("Input matches format");
+		} else {
+			System.out.println("Input doesn't match format");
+		}
+	}
+}
